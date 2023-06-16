9af428b68c350467af8870e8eaecf180cf5659cb
MOrtmann90 <mauro.ortmann@gmail.com>
2023-06-16 20:39:44 -0300

    v1

---
+++ boletines/boletin_52/README.md
@@ -0,0 +1,2 @@
+# boletin-52
+Boletín Estadístico Nº 52 de la Dirección de Estadística Local del Municipio de Tandil
--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector(".menu-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
 const menuLinks = document.querySelectorAll("a.menu-link");
+
 menuBtn.onclick = () => {
   navbar.classList.add("show");
   menuBtn.classList.add("hide");
@@ -29,3 +30,70 @@ window.onscroll = () => {
     ? navbar.classList.add("sticky")
     : navbar.classList.remove("sticky");
 };
+
+// Links routing
+
+// Section 1
+const boletinesBtn = document.querySelector("#boletines-btn");
+const eheBtn = document.querySelector("#ehe-btn");
+const informesBtn = document.querySelector("#informes-btn");
+
+boletinesBtn.addEventListener("click", () => {
+  window.open("boletines.html", "_blank");
+});
+
+eheBtn.addEventListener("click", () => {
+  window.open("http://datos.tandil.gov.ar/dataset/ehe", "_blank");
+});
+
+informesBtn.addEventListener("click", () => {
+  window.open("http://datos.tandil.gov.ar/dataset/informes-varios", "_blank");
+});
+
+// Section 2
+const autoBtn = document.querySelector("#auto-btn");
+const construccionBtn = document.querySelector("#construccion-btn");
+const comercioBtn = document.querySelector("#comercio-btn");
+const preciosBtn = document.querySelector("#precios-btn");
+const turismoBtn = document.querySelector("#turismo-btn");
+const industriaBtn = document.querySelector("#industria-btn");
+
+autoBtn.addEventListener("click", () => {
+  window.open(
+    "http://datos.tandil.gov.ar/dataset/informe-automotriz",
+    "_blank"
+  );
+});
+
+construccionBtn.addEventListener("click", () => {
+  window.open("http://datos.tandil.gov.ar/dataset/imac", "_blank");
+});
+
+comercioBtn.addEventListener("click", () => {
+  window.open(
+    "http://datos.tandil.gov.ar/dataset/comercio-minorista",
+    "_blank"
+  );
+});
+
+preciosBtn.addEventListener("click", () => {
+  window.open("http://datos.tandil.gov.ar/dataset/precios", "_blank");
+});
+
+turismoBtn.addEventListener("click", () => {
+  window.open("http://datos.tandil.gov.ar/dataset/turismo", "_blank");
+});
+
+industriaBtn.addEventListener("click", () => {
+  window.open("http://datos.tandil.gov.ar/dataset/industria", "_blank");
+});
+
+// Section 3
+const tableroBtn = document.querySelector("#tablero-btn");
+
+tableroBtn.addEventListener("click", () => {
+  window.open(
+    "http://datos.tandil.gov.ar/dataset/tablero-de-indicadores-economicos",
+    "_blank"
+  );
+});
